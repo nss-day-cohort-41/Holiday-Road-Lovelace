34b56c20a47b1155b881739351c942aba483a8fb
James Su-Shum <sushumjames@gmail.com>
2020-06-09 21:55:34 -0500

    refactored fetch weather api component

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,13 @@
 //     }
 // )
 
-// Invoke the getWeatherData function to fetch data from openWeatherAPI
-getWeatherData().then(() => {
-  // Invoke the fiveDayForecast function to begin the filter process
-  fiveDayForecast()
-  // Invoke the displayForecast function to display forecast objects in DOM
-  displayForecast()
-})
+  // Invoke the getWeatherData method in weatherApi object to fetch data from openWeatherAPI
+  weatherApi.getWeatherData().then(
+    () => {
+      // Invoke the fiveDayForecast function to begin the filter process
+      fiveDayForecast()
+      // Invoke the displayForecast function to display forecast objects in DOM
+      displayForecast()
+      console.log(weatherApi.weatherData)
+  }
+)
