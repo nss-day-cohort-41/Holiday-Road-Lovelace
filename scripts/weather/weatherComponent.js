78e298584b8c8874c5fb1aeeda2353c0a9ed30ba
James Su-Shum <sushumjames@gmail.com>
2020-06-11 15:32:01 -0500

    formatted date in weather api

--- scripts/weather/weatherComponent.js
+++ scripts/weather/weatherComponent.js
@@ -10,7 +10,11 @@ const fiveDayForecast = (weatherApi) => {
 
   for (let i = 0; i < weatherApi.list.length; i+= 8) {
 
-    const desc = `<div class="weather-card"><div class="weather-date"><h2>${weatherApi.list[i].dt_txt}</h2></div><div class=""weather-icon><img src="http://openweathermap.org/img/wn/${weatherApi.list[i].weather[0].icon}@2x.png"></div><div class="weather-description">${weatherApi.list[i].weather[0].description}</div><div class="weather-max"><h3>High: ${weatherApi.list[i].main.temp_max}&deg;</h3></div><div class="weather-min"><h3>Low: ${weatherApi.list[i].main.temp_min}&deg;</h3></div></div>`
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const d = new Date(`${weatherApi.list[i].dt_txt}`);
+    const dayName = days[d.getDay()];
+
+    const desc = `<div class="weather-card"><div class="weather-date"><h2>${dayName}</h2></div><div class=""weather-icon><img src="http://openweathermap.org/img/wn/${weatherApi.list[i].weather[0].icon}@2x.png"></div><div class="weather-description">${weatherApi.list[i].weather[0].description}</div><div class="weather-max"><h3>High: ${weatherApi.list[i].main.temp_max}&deg;</h3></div><div class="weather-min"><h3>Low: ${weatherApi.list[i].main.temp_min}&deg;</h3></div></div>`
 
     forecast.push(desc)
   }
